3506d0c4a626152348e34cd7ecddfaf296e782dc
piixx <nospacezone@gmail.com>
2017-09-30 14:13:25 -0500

    >infantile projects v1.01
    >crash sandbox A
    >>v1.01
    >>buffalowire exploit test
    >>process running
    >>computing 16, 32, and 64 bit from string
    >>opening file w/ rw perms
    >>opening directory w/ rw perms
    >>fully detailed exceptions
    >>a cool clear button
    >>no exception handling whatsoever, have all your errors
    >>in the future i'll probably put out bounties for this, like
    >>"if you can trigger X exception in X sandbox i'll do X"
    >patch db
    >>list of all patches ever for all my programs, gg

--- crash-sandbox-A/crash-sandbox-A/Form1.Designer.cs
+++ crash-sandbox-A/crash-sandbox-A/Form1.Designer.cs
@@ -28,20 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "About / Version Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Commands entered here will be ran as a process directly.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Anything entered here will be computed. (16 bit)";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(557, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1, 282);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(818, 269);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(269, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(282, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "Anything entered here will be computed. (32 bit)";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(557, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Go";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(269, 100);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(282, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Anything entered here will be computed. (64 bit)";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(557, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Go";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(346, 556);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Clear";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(269, 129);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(282, 20);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "Path of a file to open. (Read/Write)";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(557, 126);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Go";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(269, 158);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(282, 20);
+            this.textBox7.TabIndex = 13;
+            this.textBox7.Text = "Path of a file to open. (Read)";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(557, 155);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Go";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(269, 187);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(282, 20);
+            this.textBox8.TabIndex = 15;
+            this.textBox8.Text = "Path of a directory to open. (Read/Write)";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(557, 184);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "Go";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 42);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(127, 23);
+            this.button10.TabIndex = 17;
+            this.button10.Text = "Test for BuffaloWire";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Cool Bug Tests";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 578);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- crash-sandbox-A/crash-sandbox-A/Form1.cs
+++ crash-sandbox-A/crash-sandbox-A/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,207 @@ namespace crash_sandbox_A
 {
     public partial class Form1 : Form
     {
+        public static string VersionInfo =
+            "Infantile Stage\n" + // stage of development
+            "Infantile Repo Version: 1.01\n" + // inf repo version
+            "https://github.com/piixx/infantile\n" + // inf repo link
+            "> CRASH SANDBOX A <\n" + // display information about the crash sandbox
+            "> VERSION 1.01 <\n" + // version 1.01
+            "> 9/30/17 <"; // last updated
         public Form1()
         {
             InitializeComponent();
+            textBox3.ForeColor = Color.Red;
+            textBox3.Text = "------===============ERROR LIST===============-------";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(VersionInfo);
+        }
+
+        private void button2_Click(object sender, EventArgs e) // string -> process run
+        {
+            try
+            {
+                string arguments = textBox1.Text; // get the arguments needed
+                char seperator = ' '; // define the argument-process seperator as space
+                string run = arguments.Split(seperator)[0]; // split the string, first word is the process
+                string args; // set null args
+                if (arguments.Split(seperator).Length <= 1) { args = ""; } // no args? thats ok, set to "". (INF-0001)
+                else { args = arguments.Split(seperator)[1]; } // args? coolio, set them
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(run, args); // set the runtime parameters
+                Process.Start(processStartInfo); // doit
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 16bit string -> int calculator
+        {
+            try
+            {
+                // kudos https://stackoverflow.com/questions/333737/evaluating-string-342-yield-int-18
+                DataTable dt = new DataTable(); // make a new datatable of "in-memory data"
+                var v = dt.Compute(textBox2.Text, ""); // tell that data table to compute our request w/o data filter
+                int c = Convert.ToInt16(v); // convert to 16bit int
+                textBox2.Text = c.ToString(); // convert it to a string and display it
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // empty function
+        }
+
+        private void button4_Click(object sender, EventArgs e) // 32bit string -> int calculator
+        {
+            try
+            {
+                // kudos https://stackoverflow.com/questions/333737/evaluating-string-342-yield-int-18
+                DataTable dt = new DataTable(); // make a new datatable of "in-memory data"
+                var v = dt.Compute(textBox4.Text, ""); // tell that data table to compute our request w/o data filter
+                int c = Convert.ToInt32(v); // convert to 32bit int
+                textBox4.Text = c.ToString(); // if all goes well, display it
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) // 64bit string -> int calculator
+        {
+            try
+            {
+                // kudos https://stackoverflow.com/questions/333737/evaluating-string-342-yield-int-18
+                DataTable dt = new DataTable(); // make a new datatable of "in-memory data"
+                var v = dt.Compute(textBox5.Text, ""); // tell that data table to compute our request w/o data filter
+                long c = Convert.ToInt64(v); // convert to 32bit int
+                textBox5.Text = c.ToString(); // if all goes well, display it
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) // "clear" button
+        {
+            textBox3.Text = "------===============ERROR LIST===============-------";
+        }
+
+        private void button7_Click(object sender, EventArgs e) // string -> file open (rw access)
+        {
+            try
+            {
+                string filename = textBox6.Text; // set file path
+                File.OpenWrite(filename); // open w/ RW access
+                textBox6.Text = "Success";
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) // string -> file open (read access)
+        {
+            try
+            {
+                string filename = textBox6.Text; // set file path
+                File.OpenRead(filename); // open w/ strictly read access
+                textBox7.Text = "Success";
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                    textBox3.Text +
+                    Environment.NewLine +
+                    Environment.NewLine +
+                    a.ToString();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e) // string -> dir open (rw access)
+        {
+            try
+            {
+                string dirname = textBox8.Text; // set directory name
+                DateTime time = Directory.GetLastAccessTime(dirname); // retrieve last time (read)
+                Directory.SetLastAccessTime(dirname, time); // write the last time as the last time (write)
+                                                            // the tests are harmless because nothing is really changed and tests both read and write
+            }
+            catch (Exception a) // catch an exception, display it
+            {
+                textBox3.Text =
+                   textBox3.Text +
+                   Environment.NewLine +
+                   Environment.NewLine +
+                   a.ToString();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e) // test for buffalowire bug (UGLY CODE)
+        {
+            // c# variant of BuffaloWire
+            try
+            {
+                textBox3.Text =
+                   textBox3.Text +
+                   Environment.NewLine +
+                   Environment.NewLine +
+                   "-- There are a couple different scenarios for this exploit:" + Environment.NewLine +
+                   "-- Scenario 1: You are on Windows 10 Build 1703, and $MFT is a valid file." +
+                   Environment.NewLine +
+                   "-- Scenario 2: You are on Windows 10 Pre-1703, $MFT does not exist, but will not crash " +
+                   "the system. It is regarded as 'File not found.'" + Environment.NewLine +
+                   "-- Scenario 3: You are on Windows Pre-10, $MFT does not exist and will hang the system.";
+                File.GetCreationTime("c:/$MFT/test");
+                textBox3.Text =
+                   textBox3.Text +
+                   Environment.NewLine +
+                   Environment.NewLine +
+                   "--==--==--BuffaloWire does not affect this system.--==--==--" +
+                   Environment.NewLine +
+                   "--==--==--Scenario 1: You are on Windows 10 Build 1703.--==--==--";
+            }
+            catch
+            {
+                textBox3.Text =
+                   textBox3.Text +
+                   Environment.NewLine +
+                   Environment.NewLine +
+                   "--==--==--BuffaloWire does not affect this system.--==--==--" +
+                   Environment.NewLine +
+                   "--==--==--Scenario 2: You are on Windows 10 Pre-1703.--==--==--";
+            }
         }
     }
 }
